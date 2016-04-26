a5ff95d25623e4644df3819871c41c97f3285e9d
Manio <Manio@Chuj.pl>
2016-04-26 17:22:46 +0200

    Map change - Leaflets

--- src/main/webapp/js/loadMap.js
+++ src/main/webapp/js/loadMap.js
@@ -1,59 +1,32 @@
 var map;
-var infowindow;
-var lastOpenInfoWindow;
-var lastOpenMarker;
 var date = new Date;
 
 function initMap() {
-    var mapDiv = document.getElementById('map');
-    map = new google.maps.Map(mapDiv, {
-        center: {lat: 54.37884, lng: 18.46807},
-        zoom: 4
-    });
-    var location = {lat: 54.37884, lng: 18.46807};
-    addMarker(location);
+    map = L.map('map').setView([54.37884, 18.46807], 4);
+	
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		maxZoom: 18,
+		id: 'mapbox.streets',
+		accessToken: 'pk.eyJ1IjoibWFuaW96YWtvIiwiYSI6ImNpbmV4ZzFhdjAwN3l3emx5bjYyZWdranIifQ.TK1h3WP9gl91Tpirc9zIZg'
+	}).addTo(map);
+	
+    var location = [54.37884, 18.46807];
+    addMarker(location, "Test");
+	addMarker([0, 0], "Test2");
 	document.getElementById('date_label_1').value = date.getMonth() + 1;
 	document.getElementById('date_label_2').value = date.getDate();
 	document.getElementById('calendar_year').value = date.getFullYear();
 	document.getElementById('time_hours').value = date.getHours() > 10 ? date.getHours() : '0' + date.getHours();
 	document.getElementById('time_mins').value = date.getMinutes() > 10 ? date.getMinutes() : '0' + date.getMinutes();
-	
-	
 }
 
-function addMarker(location) {
-    var marker = new google.maps.Marker({
-       position: location,
-       title: "test",
-       map: map
-    });
-    var infowindow = new google.maps.InfoWindow({
-        content: "<div class=\"infoWindowForm\">" + 
-					"<h3>" + marker.title + "</h3>" +
-					"<button class=\"fromButton\" onclick=\"fromButtonClick('" + marker.title + "')\">From</button>" +
-					"<button class=\"toButton\" onclick=\"toButtonClick('" + marker.title + "')\">To</button>"
-    });
-    google.maps.event.addListener(marker, 'click', (function() {
-        if(!marker.open) {
-            infowindow.open(map, marker);
-            marker.open = true;
-            if(lastOpenInfoWindow && lastOpenInfoWindow != infowindow) {
-                lastOpenInfoWindow.close();
-            }
-            if(lastOpenMarker && lastOpenMarker != marker) {
-                lastOpenMarker.open = false;
-            }
-            lastOpenInfoWindow = infowindow;
-            lastOpenMarker = marker;
-        } else {
-            infowindow.close();
-            marker.open = false;
-        }
-        google.maps.event.addListener(map, 'click', function() {
-                infowindow.close();
-                marker.open = false;
-        });
-    }));
+function addMarker(location, content) {
+    var marker = new L.marker(location).bindPopup("<div class=\"infoWindowForm\">" + 
+					"<h3>" + content + "</h3>" +
+					"<button class=\"fromButton\" onclick=\"fromButtonClick('" + content + "')\">From</button>" +
+					"<button class=\"toButton\" onclick=\"toButtonClick('" + content + "')\">To</button>"
+    ).addTo(map);
 }
 
 function fromButtonClick(markerTitle) {
--- src/main/webapp/js/serverConnection.js
+++ src/main/webapp/js/serverConnection.js
@@ -3,8 +3,8 @@ var ws = new WebSocket("ws://192.168.99.101:32775/socket");
 // called when socket connection established
 ws.onopen = function() {
     appendLog("Connected to stock service! Press 'Start' to get stock info.");
-    var location2 = {lat: 44.37884, lng: 8.46807};
-    addMarker(location2);
+    var location2 = [44.37884, 8.46807];
+    addMarker(location2, "Test2");
 };
  
 // called when a message received from server
@@ -31,3 +31,17 @@ function appendLog(logText) {
 function sendToServer(msg) {
     ws.send(msg);
 }// establish the communication channel over a websocket
+
+function submitForm() {
+	if(ws.isOpen()) {
+		var jsonToSend = "{\"form\": { \"from\": \"" + document.getElementById('from_textbox').value + "\", " +
+			"\"to\": " + document.getElementById('to_textbox').value + "\n, " +
+			"\"date\": \"" + document.getElementById('date_label_1').value + "/" + document.getElementById('date_label_2').value + "/" +
+				document.getElementById('calendar_year').value + "\", " +
+			"\"time\": \"" + document.getElementById('time_hours').value + ":" + document.getElementById('time_mins').value + "\"" +
+		"}}";
+		sendToServer(jsonToSend);
+	} else {
+		document.getElementById('sending_error_label').value = "conncetions with server is closed";
+	}
+}
