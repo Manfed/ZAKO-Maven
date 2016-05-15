22136309ad471697ae7dd0aa7a4874f2ed31b501
dmajer <dark.marleyy@gmail.com>
2016-05-15 12:26:56 +0200

    Połączenie kontrolera REST do serwera

--- src/main/java/eti/zako/jetty/server/JettyServer.java
+++ src/main/java/eti/zako/jetty/server/JettyServer.java
@@ -1,6 +1,5 @@
 package eti.zako.jetty.server;
 
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -11,29 +10,32 @@ import eti.zako.jetty.servlets.AirportWebSocketServlet;
 public class JettyServer {
 
     public static void main(String[] args) throws Exception {
-        
+
         Server server = new Server(10000);
-        
+
         //konfiguracja servletu odpowiadajacego za wyswietlenie strony
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
-        
+
         AirportHttpServlet servlet = new AirportHttpServlet();
         ServletHolder holder = new ServletHolder("airport", servlet);
         holder.setInitParameter("resourceBase", "./src/main/webapp/");
-        
-        context.addServlet(holder, "/*");
-        
+        holder.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", "com.sun.jersey.api.core.PackagesResourceConfig");
+        holder.setInitParameter("com.sun.jersey.config.property.packages", "eti.zako.restful");
+        holder.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
+
+        context.addServlet(holder, "");
+
         //dodanie socketa
-        
+
         AirportWebSocketServlet wsServlet = new AirportWebSocketServlet();
         ServletHolder wsHolder = new ServletHolder("socket", wsServlet);
         context.addServlet(wsHolder, "/socket");
-        
+
         server.setHandler(context);
-        
+
         server.start();
         server.join();
     }
-    
+
 }
